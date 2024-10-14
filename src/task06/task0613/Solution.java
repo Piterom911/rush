17ef2de7565c05c53d3980c6ea0210f383461093
Roman Dogoda <piterom911@gmail.com>
2024-10-14 12:22:23 +0200

    task0613 done

---
+++ src/task06/task0613/Solution.java
@@ -0,0 +1,17 @@
+package task06.task0613;
+
+import java.util.Arrays;
+
+public class Solution {
+
+    public static String[] strings = new String[]{"I", "love", "Java"};
+    public static int[] ints = new int[]{1, 2, 3, 4, 5, 6, 7, 8};
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(strings));
+        System.out.println(Arrays.toString(ints));
+    }
+}
+
+// Реализуй метод main(String[]),
+// который выводит массивы strings и ints в консоли с помощью метода Arrays.toString().
