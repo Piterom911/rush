c158ae2c09bb37ae3fc2470dc8415cf8180b99ed
Roman Dogoda <piterom911@gmail.com>
2024-10-16 17:34:09 +0200

    task_07_15 done

---
+++ src/task_06/task_06_15/Solution.java
@@ -0,0 +1,14 @@
+package task_06.task_06_15;
+
+public class Solution {
+    public class Earth {
+        final public static String NAME = "Земля";
+        final public static double SQUARE  = 510_100_000;
+        final public static long POPULATION = 7_594_000_000L;
+        final public static long EQUATOR_LENGTH = 40_075_696;
+    }
+}
+
+// Перед тобой класс Earth - сборник переменных, описывающих определенные характеристики планеты Земля.
+// Сделай из переменных класса Earth глобальные константы. Не забудь переименовать переменные,
+// чтобы они отвечали стилю написания глобальных переменных.
