6540b6bb3419a68c78170b94fbd50aaadd0baf5d
Roman Dogoda <piterom911@gmail.com>
2024-10-16 12:43:44 +0200

    task_07_10 done

---
+++ src/task_06/task_06_10/Person.java
@@ -0,0 +1,25 @@
+package task_06.task_06_10;
+
+public class Person {
+    private String firstName;
+    private String lastName;
+
+    public Person(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    protected String getFirstName() {
+        return firstName;
+    }
+
+    String getLastName() {
+        return lastName;
+    }
+
+    public String getFullName() {                  // Here was the private modifier
+        return firstName + " " + lastName;
+    }
+}
+
+
