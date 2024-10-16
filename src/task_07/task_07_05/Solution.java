418420b767d08a63e2286732f7058a9c679175f7
Roman Dogoda <piterom911@gmail.com>
2024-10-16 10:57:34 +0200

    task_07_05 done

---
+++ src/task_07/task_07_05/Solution.java
@@ -0,0 +1,31 @@
+package task_07.task_07_05;
+
+public class Solution {
+    public static void main(String[] args) {
+        int[] array = {11, 22, 33, 44, 55, 66, 77, 88, 99};
+        printArray(array);
+        reverseArray(array);
+        printArray(array);
+    }
+
+    public static void reverseArray(int[] array) {
+        for (int i = 0; i < array.length / 2; i++) {
+            int frontValue = array[i];
+            int backIndex = array.length - 1 - i;
+            array[i] = array[backIndex];
+            array[backIndex] = frontValue;
+        }
+    }
+
+    public static void printArray(int[] array) {
+        for (int i : array) {
+            System.out.print(i + ", ");
+        }
+        System.out.println();
+    }
+}
+
+// Напишем утилиту для работы с массивами. Основная часть функционала готова: метод printArray()
+// выводит в консоли все элементы массива. Тебе осталась мелочь: реализовать метод reverseArray().
+// Он должен менять порядок элементов массива на обратный. Метод должен работать только
+// с массивами целочисленных значений (int[]).
