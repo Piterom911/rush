afcc01d5a9150cfccb9520c46ed5845810bb32ce
Roman Dogoda <piterom911@gmail.com>
2024-10-17 12:20:36 +0200

    task_07_01 done

---
+++ src/task_07/task_07_01/Solution.java
@@ -0,0 +1,19 @@
+package task_07.task_07_01;
+
+public class Solution {
+    public static char ch = 'l';
+    public static boolean isTrue = false;
+    public static byte bt = 127;
+    public static short shr = 32_767;
+    public static int num = 2_147_483_647;
+    public static long big = 9_223_372_036_854_775_807L;
+    public static float fl = 9.24f;
+    public static double db = 9.24209348570239845702394857d;
+
+    public static void main(String[] args) {
+    }
+}
+
+// Как тебе уже известно, всего в языке Java 8 примитивных типов.
+// Создай по одной переменной каждого примитивного типа в классе Solution.
+// Переменные не должны находиться в каком-либо методе.
