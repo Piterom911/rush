797449aabf061502842b660f6e987a617b3be923
Roman Dogoda <piterom911@gmail.com>
2024-10-17 12:30:25 +0200

    task_07_03 done

---
+++ src/task_07/task_07_03/Solution.java
@@ -0,0 +1,11 @@
+package task_07.task_07_03;
+
+public class Solution {
+    public static byte bt = -128;
+    public static short shr = -32_768;
+    public static int num = -2_147_483_648;
+    public static long big = -9_223_372_036_854_775_808L;
+}
+
+// Создай по одной переменной каждого примитивного целочисленного типа в классе Solution.
+// Этим переменным присвой минимально возможное для них значение. Никаких методов создавать не нужно.
