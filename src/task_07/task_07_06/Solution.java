c96e59126a0436d68579eeed47b79b5f4835c8ca
Roman Dogoda <piterom911@gmail.com>
2024-10-17 13:35:24 +0200

    task_07_06 done

---
+++ src/task_07/task_07_06/Solution.java
@@ -0,0 +1,14 @@
+package task_07.task_07_06;
+
+public class Solution {
+    public static void main(String[] args) {
+        div(0.0, 0.0);
+    }
+
+    public static void div(double a, double b){
+        System.out.println(b/a);
+    }
+}
+
+// В методе main вызови метод div() с таким аргументом, чтобы вывод в консоли был "NaN".
+// Код метода div() изменять нельзя.
