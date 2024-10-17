62df73cc06120eb8d815760c02c2d9f1c5be68cf
Roman Dogoda <piterom911@gmail.com>
2024-10-17 18:34:16 +0200

    task_07_12 done

---
+++ src/task_07/task_07_12/Solution.java
@@ -0,0 +1,32 @@
+package task_07.task_07_12;
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        String str1 = new String();
+        String str2 = new String();
+        String str3 = new String();
+        String str4 = new String();
+        String str5 = new String();
+        String str6 = new String();
+        String str7 = new String();
+        String str8 = new String();
+        String str9 = new String();
+        String str10 = new String();
+
+        int[] arr1 = new int[5];
+        int[] arr2 = new int[5];
+        int[] arr3 = new int[5];
+        int[] arr4 = new int[5];
+        int[] arr5 = new int[5];
+
+        Scanner console1 = new Scanner(System.in);
+        Scanner console2 = new Scanner(System.in);
+    }
+}
+
+// Используя оператор new в методе main(), создай:
+// 10 объектов типа String;
+// 5 объектов типа int[] (массивов значений int);
+// 2 объекта типа Scanner.
