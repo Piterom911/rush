a62f51c568622a4d1f1e44141ab9c730e9d96b8f
Roman Dogoda <piterom911@gmail.com>
2024-10-17 14:17:56 +0200

    task_07_08 done

---
+++ src/task_07/task_07_08/Solution.java
@@ -0,0 +1,22 @@
+package task_07.task_07_08;
+
+public class Solution {
+    public static byte a;
+    public static short b;
+    public static int c;
+    public static long d;
+
+    public static void main(String[] args) {
+        setValues(100);
+    }
+
+    public static void setValues(long value){
+        a = (byte) value;
+        b = (short) value;
+        c = (int) value;
+        d = value;
+    }
+}
+
+// Реализуй метод setValues(long value), чтобы он устанавливал полученное значение параметра value переменным a, b, c и d.
+// Изменять типы переменных a, b, c и d нельзя.
