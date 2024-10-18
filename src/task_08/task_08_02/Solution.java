45f0e09842eec31ae4688eab15baa2de1a1bdc13
Roman Dogoda <piterom911@gmail.com>
2024-10-18 21:47:27 +0200

    task_08_02 done

---
+++ src/task_08/task_08_02/Solution.java
@@ -0,0 +1,20 @@
+package task_08.task_08_02;
+
+public class Solution {
+    public static double sqrt(double a) {
+        return Math.sqrt(a);
+    }
+
+    public static double cbrt(double a) {
+        return Math.cbrt(a);
+    }
+
+    public static double pow(int number, int power) {
+        return Math.pow(number, power);
+    }
+}
+
+// Исправь методы класса Solution, используя класс Math:
+// sqrt(double) — должен возвращать квадратный корень переданного аргумента.
+// cbrt(double) — должен возвращать кубический корень переданного аргумента.
+// pow(int, int) — должен возвращать значение первого аргумента, возведенного в степень второго аргумента.
