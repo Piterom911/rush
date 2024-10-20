fbc1ba82b17d9c1e3f95ecf49bccc3b04d62470b
Roman Dogoda <piterom911@gmail.com>
2024-10-20 20:31:07 +0200

    task_08_07 done

---
+++ src/task_08/task_08_07/Solution.java
@@ -0,0 +1,15 @@
+package task_08.task_08_07;
+
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+public class Solution {
+    public static void main(String[] args) {
+        LinkedList<String> linkedList = new LinkedList<>();
+        InputStreamReader inputStreamReader = new InputStreamReader(System.in);
+        AtomicBoolean atomicBoolean = new AtomicBoolean(false);
+    }
+}
+
+// В классе Solution замени все полные имена классов в коде на короткие.
