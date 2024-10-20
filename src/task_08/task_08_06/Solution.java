2a552221c8c41f8cc7586c85cb65c50492baef90
Roman Dogoda <piterom911@gmail.com>
2024-10-20 20:28:32 +0200

    task_08_06 done

---
+++ src/task_08/task_08_06/Solution.java
@@ -0,0 +1,12 @@
+package task_08.task_08_06;
+
+public class Solution {
+    public static void main(String[] args) {
+        java.util.ArrayList<java.lang.String> arrayList = new java.util.ArrayList<>();
+        java.io.BufferedReader bufferedReader = new java.io.BufferedReader(new java.io.StringReader(""));
+        java.math.BigInteger bigInteger = new java.math.BigInteger("123");
+    }
+}
+
+
+// В классе Solution замени все короткие имена классов в коде на полные.
