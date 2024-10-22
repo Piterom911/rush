384df3bbc5f37e6b2def7c3bbe5a3f6a8ae266f4
Roman Dogoda <piterom911@gmail.com>
2024-10-22 11:00:32 +0200

    task_08_16 done

---
+++ src/task_08/task_08_16/Solution.java
@@ -0,0 +1,10 @@
+package task_08.task_08_16;
+
+public class Solution {
+    public static void main(String[] args) {
+        int number = 2;
+        System.out.println(number + number * (number + ++number));
+    }
+}
+
+// Внеси исправления в метод main(), чтобы он выводил в консоли число 12.
