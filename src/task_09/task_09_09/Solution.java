08653d65df4a3b2e34f70bca528432609848e39c
Roman Dogoda <piterom911@gmail.com>
2024-10-24 11:47:28 +0200

    task_09_09 done

--- src/task_09/task_09_09/Solution.java
+++ src/task_09/task_09_09/Solution.java
@@ -1,7 +1,10 @@
 package task_09.task_09_09;
 
 public class Solution {
-
+    public static void main(String[] args) {
+        System.out.println("It's a Windows path: \"C:\\Program Files\\Java\\jdk-13.0.0\\bin\"");
+        System.out.println("It's a Java string: \\\"C:\\\\Program Files\\\\Java\\\\jdk-13.0.0\\\\bin\\\"");
+    }
 }
 
 // Выведи на экран следующий текст в две строки: It's a Windows path: "C:\Program Files\Java\jdk-13.0.0\bin"
