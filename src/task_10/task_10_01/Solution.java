2bb0398bc9fcf87cc7335e4708522bdb77b283fe
Roman Dogoda <piterom911@gmail.com>
2024-10-25 18:02:43 +0200

    task_10_01 done

---
+++ src/task_10/task_10_01/Solution.java
@@ -0,0 +1,15 @@
+package task_10.task_10_01;
+
+public class Solution {
+    public Solution() {
+        System.out.println("Небоскреб построен.");
+    }
+
+    public static void main(String[] args) {
+        Solution sscr = new Solution();
+    }
+}
+
+// Попробуем сконструировать свой первый небоскреб и объявить о результате выполнения в консоли.
+// Для этого тебе нужно вызвать конструктор класса Skyscraper в методе main и в теле конструктора вывести
+// в консоль текст "Небоскреб построен.".
