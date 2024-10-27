ba0897a07cdfbdacfd7d57efeb0b264b3eb8ba53
Roman Dogoda <piterom911@gmail.com>
2024-10-27 11:51:19 +0100

    task_10_20 done

---
+++ src/task_10/task_10_20/Computer.java
@@ -0,0 +1,11 @@
+package task_10.task_10_20;
+
+public class Computer {
+    Mouse mouse = new Mouse();
+    Keyboard keyboard = new Keyboard();
+    Monitor monitor = new Monitor();
+    SystemUnit systemUnit = new SystemUnit();
+}
+
+// Попробуем собрать компьютер. У нас есть классы: системный блок, монитор, клавиатура, мышь.
+// В классе компьютер в конструкторе создай объекты классов-составляющих компьютера.
---
+++ src/task_10/task_10_20/Mouse.java
@@ -0,0 +1,4 @@
+package task_10.task_10_20;
+
+public class Mouse {
+}
