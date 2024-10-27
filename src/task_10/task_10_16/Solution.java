31f4380b912dea26595c2a1cd353da7ab7f7825a
Roman Dogoda <piterom911@gmail.com>
2024-10-27 10:51:21 +0100

    task_10_16 done

---
+++ src/task_10/task_10_16/Solution.java
@@ -0,0 +1,18 @@
+package task_10.task_10_16;
+
+public class Solution {
+    public static void showWeather(City city) {
+        System.out.println("В городе " + city.getName() + " сегодня температура воздуха " + city.getTemperature());
+    }
+
+    public static void main(String[] args) {
+        City city = new City("Дубай", 40);
+        showWeather(city);
+    }
+}
+
+// У класса City есть два поля - название города и текущая температура воздуха. Также в классе Solution есть метод
+// showWeather: он выводит в консоли информацию о погоде в городе, который передан в качестве аргумента метода.
+// Реализуй метод showWeather, который должен выводить в консоли информацию в таком формате: В городе "название города"
+// сегодня температура воздуха "температура в городе" В методе main один раз нужно вызвать метод showWeather
+// с нужным аргументом.
