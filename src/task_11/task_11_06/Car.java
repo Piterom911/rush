1e43f98f1ceb9188b60c1a0ae93d7447e14fb08b
Roman Dogoda <piterom911@gmail.com>
2024-10-28 11:00:55 +0100

    task_11_06 done

---
+++ src/task_11/task_11_06/Car.java
@@ -0,0 +1,31 @@
+package task_11.task_11_06;
+
+/* 
+Одинаковые машины
+*/
+
+public class Car {
+    private String modelName;
+    private int speed;
+
+    public String getModelName() {
+        return modelName;
+    }
+
+    public void setModelName(String modelName) {
+        this.modelName = modelName;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+}
+
+// В далеком-далеком прошлом все машины называли одинаково. Но вот в один прекрасный день директору завода по
+// производству автомобилей надоело однообразие, и он выдал распоряжение: давать разные названия моделям авто.
+// Итак, как ты уже догадался, твоя задача — помочь главному инженеру исправить программу по производству машин так,
+// чтобы каждому объекту-машине можно было присвоить уникальное имя.
