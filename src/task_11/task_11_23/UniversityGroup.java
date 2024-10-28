18d96d44d8d02aac11cb522644b172f3ef312630
Roman Dogoda <piterom911@gmail.com>
2024-10-28 15:30:19 +0100

    task_11_23 done

---
+++ src/task_11/task_11_23/UniversityGroup.java
@@ -0,0 +1,41 @@
+package task_11.task_11_23;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/* 
+Почувствуй себя деканом
+*/
+
+public class UniversityGroup {
+    public List<String> students;
+
+    public UniversityGroup() {
+        students = new ArrayList<>();
+        students.add("Сергей Фрединский");
+        students.add("Виталий Правдивый");
+        students.add("Максим Козыменко");
+        students.add("Наталия Андрющенко");
+        students.add("Ира Величенко");
+        students.add("Николай Соболев");
+        students.add("Снежана Слободенюк");
+    }
+
+    public void exclude(String excludedStudent) {
+        ArrayList<String> copy = new ArrayList<>(students);
+        for (String student : copy) {
+            if (student.equals(excludedStudent)) {
+                students.remove(student);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        UniversityGroup universityGroup = new UniversityGroup();
+        universityGroup.exclude("Виталий Правдивый");
+        universityGroup.students.forEach(System.out::println);
+    }
+}
+
+// У нас есть группа в университете и метод, который должен отчислять конкретного студента, но он почему-то не работает.
+// Твоя задача — найти ошибку и исправить, чтобы метод exclude смог отчислить студента.
