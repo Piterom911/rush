aee1bdff8928496af9e5534d2e73b649afab8124
Roman Dogoda <piterom911@gmail.com>
2024-10-28 15:41:37 +0100

    task_11_26 done

---
+++ src/task_11/task_11_26/Solution.java
@@ -0,0 +1,12 @@
+package task_11.task_11_26;
+
+/* 
+Студент
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Student student = new Student("Амиго");
+        System.out.println(student.name);
+    }
+}
