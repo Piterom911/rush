c11038830276d660da944ae9dc9cf1db7ea81c05
Roman Dogoda <piterom911@gmail.com>
2024-10-28 12:09:35 +0100

    task_11_12 done

---
+++ src/task_11/task_11_12/Solution.java
@@ -0,0 +1,16 @@
+package task_11.task_11_12;
+
+/* 
+Кон, котэ и нация
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        String name = "Diego";
+        int age = 35;
+        System.out.println("Name : " + name + " Age : " + age);
+    }
+}
+
+// "Кон", "котэ" и "нация". Как соединить эти три строки? Ты же помнишь, как соединять String'и в Java?
+// Исправь ошибки в коде, чтобы программа компилировалась и работала.
