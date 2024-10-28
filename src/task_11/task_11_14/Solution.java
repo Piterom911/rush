2b8b272e6f58fa6e289486aa8a7f767a23a8617d
Roman Dogoda <piterom911@gmail.com>
2024-10-28 12:17:58 +0100

    task_11_14 done

---
+++ src/task_11/task_11_14/Solution.java
@@ -0,0 +1,19 @@
+package task_11.task_11_14;
+
+/* 
+И снова скобки
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        int number = 1;
+        if (number < 5) {
+            System.out.println("number < 5");
+        } else {
+            System.out.println();
+        }
+    }
+}
+
+// Кажется, в этот раз проблема снова со скобками. Но уже с другими. Исправь ошибки в коде,
+// чтобы программа компилировалась и работала.
