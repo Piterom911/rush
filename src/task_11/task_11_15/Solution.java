dd0a3b6a7c5f05cb52a324161a1ced659b125850
Roman Dogoda <piterom911@gmail.com>
2024-10-28 12:19:34 +0100

    task_11_15 done

---
+++ src/task_11/task_11_15/Solution.java
@@ -0,0 +1,13 @@
+package task_11.task_11_15;
+
+/* 
+Многострадальный метод main
+*/
+
+public class Solution {
+    public static void main(String... errors) {
+        System.out.println("It works!");
+    }
+}
+
+// Сделай одно исправление в коде, чтобы программа запускалась.
