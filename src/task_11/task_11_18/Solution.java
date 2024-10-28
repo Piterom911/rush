d748da3bb9e1b7aaa2f4fa0bfc52599967a21d52
Roman Dogoda <piterom911@gmail.com>
2024-10-28 12:25:17 +0100

    task_11_18 done

---
+++ src/task_11/task_11_18/Solution.java
@@ -0,0 +1,18 @@
+package task_11.task_11_18;
+
+/* 
+Импортный код
+*/
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        if (console.hasNext()) {
+            System.out.println(console.next());
+        }
+    }
+}
+
+// Исправь ошибку в коде, чтобы программа компилировалась и работала.
