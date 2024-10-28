98f8fbae02fbea9234896e4b79d6f0062643f212
Roman Dogoda <piterom911@gmail.com>
2024-10-28 12:06:47 +0100

    task_11_10 done

---
+++ src/task_11/task_11_10/Solution.java
@@ -0,0 +1,16 @@
+package task_11.task_11_10;
+
+/* 
+Точка и запятая
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        int number = 1;
+        System.out.println(number);
+    }
+}
+
+// Точки и запятые важны не только в пунктуации в обычном тексте.
+// Исправь ошибки в коде,
+// чтобы программа компилировалась и работала.
