e9442255ff3468dddbf3e3cf2e6e4929682c93d1
Roman Dogoda <piterom911@gmail.com>
2024-10-28 15:17:35 +0100

    task_11_20 done

---
+++ src/task_11/task_11_20/Solution.java
@@ -0,0 +1,19 @@
+package task_11.task_11_20;
+
+import java.util.Scanner;
+
+/* 
+Все буквы маленькие
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        String line = console.nextLine();
+        line = line.toLowerCase();
+        System.out.println(line);
+    }
+}
+
+// Программа должна считать с консоли строку и вывести ее на экран в нижнем регистре.
+// Исправь ошибку в коде, чтобы программа работала корректно.
