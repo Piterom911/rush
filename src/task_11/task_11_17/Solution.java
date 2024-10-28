4add2c8852de0d0061bae7db1e1aa6f2da512fbc
Roman Dogoda <piterom911@gmail.com>
2024-10-28 12:23:53 +0100

    task_11_17 done

---
+++ src/task_11/task_11_17/Solution.java
@@ -0,0 +1,13 @@
+package task_11.task_11_17;
+
+/*
+Пакет нужен?
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println("TestPackage");
+    }
+}
+
+// Исправь ошибку в коде, чтобы программа компилировалась и работала.
