f4899d57bb7801f145b64d372d508f28ab21dd7e
Roman Dogoda <piterom911@gmail.com>
2024-10-28 12:16:17 +0100

    task_11_13 done

---
+++ src/task_11/task_11_13/Solution.java
@@ -0,0 +1,15 @@
+package task_11.task_11_13;
+
+/* 
+Следим за скобками
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        if (System.getProperty("os.name").toLowerCase().contains("windows")) {
+            System.out.println("Windows detected!");
+        }
+    }
+}
+
+// Кажется, у нас есть лишняя скобка? Или одной не хватает? Разберись-ка с этой проблемой.
