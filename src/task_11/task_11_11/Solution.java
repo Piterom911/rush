b6ce6b50609779c682576d81f1e74ea4f39e5060
Roman Dogoda <piterom911@gmail.com>
2024-10-28 12:08:14 +0100

    task_11_11 done

---
+++ src/task_11/task_11_11/Solution.java
@@ -0,0 +1,15 @@
+package task_11.task_11_11;
+
+/* 
+Ох уж эти кавычки
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        String string = "Amigo";
+        System.out.println(string);
+    }
+}
+
+// В коде уже есть кавычки, но, как оказалось, их недостаточно.
+// Исправь ошибки в коде, чтобы программа компилировалась и работала.
