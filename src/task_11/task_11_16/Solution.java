402611c4f1877ff4b89b1eeb6e285901fb1975cb
Roman Dogoda <piterom911@gmail.com>
2024-10-28 12:22:04 +0100

    task_11_16 done

---
+++ src/task_11/task_11_16/Solution.java
@@ -0,0 +1,17 @@
+package task_11.task_11_16;
+
+/* 
+Правильный класс
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Cat cat = new Cat();
+        System.out.println(cat.name);
+    }
+}
+class Cat {
+    String name = "Tom";
+}
+
+// Исправь ошибки в коде, чтобы программа компилировалась и работала.
