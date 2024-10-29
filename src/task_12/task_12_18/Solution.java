7d9de5c34c4021c21d8ca9783c685281d8ce9fc7
Roman Dogoda <piterom911@gmail.com>
2024-10-29 18:55:32 +0100

    task_12_18 done

---
+++ src/task_12/task_12_18/Solution.java
@@ -0,0 +1,26 @@
+package task_12.task_12_18;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
+/* 
+Shine bright like a diamond
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        ArrayList<String> stringArrayList = new ArrayList<>();
+        Stack<String> stack = new Stack<>();
+        Map<String, ArrayList<String>> map = new HashMap<>();
+        var exceptionsList = new ArrayList<Exception>();
+        var filesStack = new Stack<File>();
+    }
+}
+
+// В методе main создаются объекты разных типов. В некоторых случаях тип элементов объекта можно опустить и просто
+// использовать оператор diamond(<>). Замени тип элементов объекта на оператор diamond там, где это возможно, не меняя
+// левую часть выражений. Порядок следования переменных не должен меняться.
