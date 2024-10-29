d128c83010976bf798369f9747466fdb91d9a1e9
Roman Dogoda <piterom911@gmail.com>
2024-10-29 18:51:55 +0100

    task_12_17 done

---
+++ src/task_12/task_12_17/Solution.java
@@ -0,0 +1,28 @@
+package task_12.task_12_17;
+
+/* 
+Назад в прошлое
+*/
+
+public class Solution {
+    private int[] intArray = new int[4];
+
+    public int[] getIntArray() {
+        return intArray;
+    }
+
+    public void setIntArray(int[] intArray) {
+        this.intArray = intArray;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        String  string = new String("Hello");
+        StringBuilder stringBuilder = new StringBuilder();
+    }
+}
+
+// В классе Solution есть поле intArray, геттер и сеттер для него, и метод main с объявленными в нем переменными.
+// Тебе нужно заменить типы переменных с var на конкретный тип объекта (изменять названия и инициализацию
+// объектов нельзя), а также починить геттер и сеттер поля intArray.
+// Пример: var console = new Scanner(System.in); Заменить на: Scanner console = new Scanner(System.in);
