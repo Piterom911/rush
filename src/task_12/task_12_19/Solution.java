8a6f6954df74162fbf59db97603f0aacab0084e6
Roman Dogoda <piterom911@gmail.com>
2024-10-29 18:58:59 +0100

    task_12_19 done

---
+++ src/task_12/task_12_19/Solution.java
@@ -0,0 +1,23 @@
+package task_12.task_12_19;
+
+import java.util.ArrayList;
+
+/* 
+Двойные фигурные скобки
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        var strings = new ArrayList<String>(){{
+            add("Так");
+            add("тоже");
+            add("можно");
+            add("делать");
+            add("!");
+        }};
+    }
+}
+
+// В методе main создается список ArrayList<String>, присваивается переменной var strings и заполняется пятью элементами.
+// Нужно переписать код, не изменяя логику, используя двойные фигурные скобки.
