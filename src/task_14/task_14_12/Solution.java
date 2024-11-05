c04e9560022adb415171efdae1e2d40636bc0992
Roman Dogoda <piterom911@gmail.com>
2024-11-05 19:18:52 +0100

    task_14_12 done

---
+++ src/task_14/task_14_12/Solution.java
@@ -0,0 +1,24 @@
+package task_14.task_14_12;
+
+/* 
+Группировка исключений
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        try {
+            System.out.println("Программа работает от забора");
+            Thread.sleep(1000);
+            System.out.println("до обеда");
+        } catch (NullPointerException | NumberFormatException e) {
+            System.out.println("Произошло исключение на букву N");
+        }  catch (IllegalArgumentException | IllegalStateException | InterruptedException e) {
+            System.out.println("Произошло исключение на букву I");
+        } catch (ClassCastException e) {
+            System.out.println("Произошло исключение на букву C");
+        }
+    }
+}
+
+// Объедини блоки catch с одинаковым кодом.
