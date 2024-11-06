9ac7c54d0f50232d6bd6812c38e2417c55e12348
Roman Dogoda <piterom911@gmail.com>
2024-11-06 20:08:17 +0100

    task_15_02 done

---
+++ src/task_15/task_15_02/Solution.java
@@ -0,0 +1,27 @@
+package task_15.task_15_02;
+
+import java.util.Scanner;
+
+/* 
+Возврат к истокам
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scanner = null;
+
+        try {
+            scanner = new Scanner(System.in);
+            String line = scanner.nextLine();
+            System.out.println(line.toUpperCase());
+        } catch (Exception e) {
+            System.out.println("Something went wrong : " + e);
+        } finally {
+            if (scanner != null) scanner.close();
+        }
+    }
+}
+
+// Программа считывает с консоли строку и выводит ее на экран в верхнем регистре. Но оказывается, она не будет работать
+// на старых версиях Java (ниже 7й версии). Не меняя функциональности программы, перепиши блок try-with-resources на
+// обычный try-catch. Не забудь о вызове метода close() для объектов с внешними ресурсами.
